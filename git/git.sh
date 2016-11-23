643c71126332df10aedb6e077d7757d3bf254aa1
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-11-23 15:56:32 +0100

    Minor clean up

--- git/git.sh
+++ git/git.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-function g-lg {
+function git-lg {
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 }
 
-function g-ac {
+function git-ac {
 	git add -A; git commit -m $argv[1]
 }
 
-function g-branch {
+function git-branch {
 	git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
 }
