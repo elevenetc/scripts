58b4d4f98f58e07125b3efb3e75980777d660750
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-12-06 12:21:19 +0100

    Add ssh copy key function

--- ssh/ssh.sh
+++ ssh/ssh.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-function ssh-copy-public-key {
+function ssh-push-public-key {
     validate-param "name" $1
     validate-param "host" $2
     ssh-copy-id ${1}@${2}
+}
+
+function ssh-copy-public-key {
+    pbcopy < ~/.ssh/id_rsa.pub
 }
