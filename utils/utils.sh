9adca083f34f574532d747adabb9705893cf13b7
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-12-09 11:27:07 +0100

    Add os check function

--- utils/utils.sh
+++ utils/utils.sh
@@ -3,3 +3,31 @@
 function utils-find-text {
 	grep -r ${argv} ./
 }
+
+function utils-is-osx {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+function utils-is-linux {
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        return 1
+    else
+        return 0
+    fi
+}
+
+# Samples
+
+#function checkOsSample {
+#    utils-is-osx; isMacOS=$?
+#    echo ${isMacOS}
+#    if [[ "${isMacOS}" == '1' ]]; then
+#        echo 'true'
+#    else
+#        echo 'false'
+#    fi
+#}
