02f4d7010d2facdd074d8199e55794dd8cfd57e9
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2016-12-30 20:34:43 +0100

    Update hockeyapp utils

--- ci/hockeyapp_utils.py
+++ ci/hockeyapp_utils.py
@@ -5,22 +5,21 @@ from argparse import Namespace
 
 import requests
 
+# master
+# release/0.0.0
+# feature/APP-000-name
+# fix/APP-000-name
+prefixes = ['production', 'release', 'feature', 'fix']
 
-def load_versions(app_id, hockey_token):
-    url = 'https://rink.hockeyapp.net/api/2/apps/' + app_id + '/app_versions'
-    headers = {'X-HockeyAppToken': hockey_token}
-    response = requests.get(url, headers=headers)
-    return response.content
 
-
-def load_apps(hockey_token):
+def load_all_apps(hockey_token):
     url = 'https://rink.hockeyapp.net/api/2/apps'
     headers = {'X-HockeyAppToken': hockey_token}
     response = requests.get(url, headers=headers)
     return json.loads(response.content, object_hook=lambda d: Namespace(**d)).apps
 
 
-def filter_apps(apps, app_name):
+def filter_android_apps_by_name(apps, app_name):
     result = []
     for app in apps:
         if app.platform == 'Android' and str(app.title).startswith(app_name):
@@ -82,27 +81,84 @@ def checkout_all_remote_branches():
     return result
 
 
+# created app should have titles:
+# "Hello - production"
+# "Hello - release/v1.1"
+# "Hello - feature/JIRA-222-description"
+# "Hello - fix/JIRA-222-description"
 def filter_not_created_apps(app_name, apps, all_branches):
     result = []
-    i = len(all_branches)
+    i = len(all_branches) - 1
 
     while i >= 0:
+
+        branch = all_branches[i]
         i -= 1
-        branch = all_branches[len(all_branches) - 1]
 
-        print 'check branch:' + branch
+        for prefix in prefixes:
+            # check if branch has proper prefix
+            if branch.find(prefix) == 0:
+                # check if app created for this branch
+                created = False
+                for app in apps:
+                    if app.title == get_title(app_name, branch):
+                        created = True
+                        break
 
-        if branch.find('feature') == -1:
-            print 'invalid branch: ' + branch
-            all_branches.remove(branch)
-            continue
-
-        for app in apps:
-
-            app_title = str(app.title)
-
-            if app_title.startswith(app_name + '-' + branch):
-                print 'already created:' + branch
-                all_branches.remove(branch)
+                if not created:
+                    result.append(branch)
 
     return result
+
+
+def create_app(app_name, branch, hockey_token):
+    url = 'https://rink.hockeyapp.net/api/2/apps/new'
+    params = {
+        'title': get_title(app_name, branch),
+        'bundle_identifier': app_name,
+        'platform': 'Android',
+        'release_type': 1
+    }
+    headers = {'X-HockeyAppToken': hockey_token}
+    response = requests.get(url, headers=headers, params=params)
+    return json.loads(response.content, object_hook=lambda d: Namespace(**d))
+
+
+def load_versions(app, hockey_token):
+    pub_id = app.public_identifier
+    url = 'https://rink.hockeyapp.net/api/2/apps/' + pub_id + '/app_versions'
+    headers = {'X-HockeyAppToken': hockey_token}
+    response = requests.get(url, headers=headers)
+    return json.loads(response.content, object_hook=lambda d: Namespace(**d)).app_versions
+
+
+def load_or_create_app(app_name, branch, hockey_token):
+    apps = load_all_apps(hockey_token)
+    apps = filter_android_apps_by_name(apps, app_name)
+    app = is_app_created(app_name, branch, apps)
+
+    if app is None:
+        app = create_app(app_name, branch, hockey_token)
+
+    return app
+
+
+def assemble(build_type):
+    branch_cmd = sb.Popen(('./gradlew assemble' + build_type.capitalize()).split(), stdout=sb.PIPE)
+    return branch_cmd.stdout.readlines()
+
+
+def is_app_created(app_name, branch, apps):
+    for app in apps:
+        if app.title == get_title(app_name, branch):
+            return app
+    return None
+
+
+def get_last_commit_hash(branch):
+    hash = sb.Popen(('git log -n 1 ' + branch + ' --pretty=format:"%H"').split(), stdout=sb.PIPE)
+    return hash.stdout.readlines()[0].replace('"', '')
+
+
+def get_title(app_name, branch):
+    return app_name + ' - ' + branch
