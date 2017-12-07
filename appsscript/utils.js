bc19d25db818f0637fb2e911f3f5611543b71e6d
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2017-12-07 15:55:08 +0100

    Add utils apps script

---
+++ appsscript/utils.js
@@ -0,0 +1,33 @@
+/**
+ * Accepts instance of Date
+ */
+function sameDay(d1, d2) {
+    if (!(d1 instanceof Date) || !(d2 instanceof Date)) throw 'dates should be instance of Date, but was d1: ' + d1 + ' d2: ' + d2
+
+    return d1.getFullYear() === d2.getFullYear() &&
+        d1.getMonth() === d2.getMonth() &&
+        d1.getDate() === d2.getDate()
+}
+
+/**
+ * Returns index of column from row range
+ *
+ * @param datesRange is String like 'A1:A100'
+ */
+function getCurrentDateCol(datesRange) {
+
+    var sheet = SpreadsheetApp.getActiveSpreadsheet();
+    var currentDate = new Date();
+    var datesRow = sheet.getRange(datesRange);
+    var datesValues = datesRow.getValues()[0];
+    var count = datesRow.getWidth();
+    for (var d = 0; d < count; d++) {
+        var date = datesValues[d];
+        if (!(date instanceof Date)) continue;//if cell is not filled with data
+
+        if (sameDay(date, currentDate)) {
+            return d
+        }
+    }
+    return -1
+}
