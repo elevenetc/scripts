17c6823f18d520499397aa66596c52045cf03dfb
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-03-26 14:33:20 +0200

    Update git.sh

--- git/git.sh
+++ git/git.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-function git-lg {
+function git-log {
 	git log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
 }
 
-function git-ac {
+function git-rename-last-commit {
+    git commit --ammend
+}
+
+function git-add-and-commit {
 	git add -A; git commit -m $argv[1]
 }
 
-function git-branch {
+function git-branches-info {
 	git for-each-ref --sort=committerdate refs/heads/ --format='%(HEAD) %(color:yellow)%(refname:short)%(color:reset) - %(color:red)%(objectname:short)%(color:reset) - %(contents:subject) - %(authorname) (%(color:green)%(committerdate:relative)%(color:reset))'
 }
 
