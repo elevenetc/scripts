cc531204d1479fa6dcdcf2b30560955ec39377ab
Eugene Levenetc <shumvlesu@gmail.com>
2018-04-04 18:57:25 +0200

    Add adb screen pull, text input, tap input

--- android/and.sh
+++ android/and.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-function and-adb-screen-pull {
+function adb-adb-screen-pull {
 
     dir=~/android-screenshots/
     name=$(date).png
@@ -14,16 +14,16 @@ function and-adb-screen-pull {
 	finish-with-success
 }
 
-function and-adb-restart {
+function adb-adb-restart {
     adb kill-server; adb start-server
 }
 
-function and-adb-uninstall-app {
+function adb-adb-uninstall-app {
     validate-param "package name" $1
     adb shell pm uninstall ${1}
 }
 
-function and-is-app-installed {
+function adb-is-app-installed {
     validate-param "package name" $1
     if [ -z $(adb shell pm list packages | grep ${1}) ]; then
         green-prefix-message "${1}" " is not installed"
@@ -32,24 +32,24 @@ function and-is-app-installed {
     fi
 }
 
-function and-sys-version {
+function adb-sys-version {
     adb shell getprop ro.build.version.release
 }
 
-function and-kill-app {
+function adb-kill-app {
     validate-param "package name" $1
     adb shell am force-stop ${1}
 }
 
-function and-get-android-id {
+function adb-get-android-id {
     adb shell content query --uri content://settings/secure --where "name=\'android_id\'"
 }
 
-function and-delete-android-id {
+function adb-delete-android-id {
     adb shell content delete --uri content://settings/secure --where "name=\'android_id\'"
 }
 
-function and-set-android-id {
+function adb-set-android-id {
     validate-param "AndroidID" $1
     adb shell content insert --uri content://settings/secure --bind name:s:android_id --bind value:s:${1}
 }
@@ -58,4 +58,24 @@ function adb-pull-prefs {
     validate-param "package name" $1
     validate-param "prefs xml file name" $2
     adb exec-out run-as ${1} cat /data/data/${1}/shared_prefs/${2}.xml
+}
+
+function adb-input-text {
+    validate-param "text to input" $1
+    adb exec-out input text $1
+}
+
+function adb-input-tap {
+    validate-param "x location" $1
+    validate-param "y location" $2
+    adb exec-out input tap $1 $2
+}
+
+function adb-pull-screen-xml {
+    adb shell uiautomator dump
+    adb pull /sdcard/window_dump.xml
+    adb shell rm /sdcard/window_dump.xml
+    tidy -xml -i window_dump.xml > screen-layout.xml
+    rm window_dump.xml
+    open screen-layout.xml
 }
