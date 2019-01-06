1bd01a658a6ac8b17345ed8eba4b1e82f459a5d0
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2019-01-06 02:18:15 +0100

    Add osx util: show/hide hidden files

--- osx/osx.sh
+++ osx/osx.sh
@@ -6,3 +6,13 @@ function osx-show-notification {
     params="display notification \"${title}\" with title \"${message}\""
 	osascript -e ${params}
 }
+
+function osx-show-hidden-files {
+    defaults write com.apple.finder AppleShowAllFiles YES
+    killall Finder /System/Library/CoreServices/Finder.app
+}
+
+function osx-hide-hidden-files {
+    defaults write com.apple.finder AppleShowAllFiles NO
+    killall Finder /System/Library/CoreServices/Finder.app
+}
