e54acf22be0a45f6b3f5612d4dc10c340ad1c063
eugene.levenetc <eugene.levenetc@themobilecompany.com>
2019-02-06 15:10:08 +0100

    Add format xml util

--- format/format.sh
+++ format/format.sh
@@ -9,3 +9,8 @@ function format-json-clipboard {
     pbpaste | python -mjson.tool | pbcopy
     echo "${GREEN}Formatted result is in clipboard.${NC}"
 }
+
+function format-xml-clipboard() {
+    pbpaste | python -c 'import sys;import xml.dom.minidom;s=sys.stdin.read();print(xml.dom.minidom.parseString(s).toprettyxml())' | pbcopy
+    echo "${GREEN}Formatted result is in clipboard.${NC}"
+}
