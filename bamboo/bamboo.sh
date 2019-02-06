e588a70fda5b127ec68271eee46c428b1dcf05ff
eugene.levenetc <eugene.levenetc@themobilecompany.com>
2019-02-06 15:30:13 +0100

    Add bamboo-rest-get-available-resources

--- bamboo/bamboo.sh
+++ bamboo/bamboo.sh
@@ -4,5 +4,14 @@ function bamboo-rest-get-plans() {
     validate-param "user name" $1
     validate-param "password" $2
     validate-param "bamboo host" $3
-    curl --user {$1}:{$2} http://$3/rest/api/latest/plan?os_authType=basic
+    curl --user $1:$2 "http://{$3}/rest/api/latest/plan?os_authType=basic" | pbcopy
+    format-xml-clipboard
+}
+
+function bamboo-rest-get-available-resources() {
+    validate-param "user name" $1
+    validate-param "password" $2
+    validate-param "bamboo host" $3
+    curl --user $1:$2 "http://{$3}/rest/api/latest/?os_authType=basic" | pbcopy
+    format-xml-clipboard
 }
