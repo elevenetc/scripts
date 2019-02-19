9992e64d3f666791e01862cce8a91d7429bb274b
eugene.levenetc <eugene.levenetc@themobilecompany.com>
2019-02-19 20:22:52 +0100

    Update docker utils

--- docker/docker.sh
+++ docker/docker.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-function docker-image-shell {
+function docker-shell {
     validate-param "image name" $1
     docker exec -it ${1} bash
+}
+
+function docker-shell-mysql {
+    docker exec -it mysql bash -l
 }
 
 function docker-create-mysql {
     docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=password -d mysql:latest
 }
+
